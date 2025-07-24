891f8dfe5736347cbc8fe993b224d3712f4a1851
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-07-24 21:34:54 +0530

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white bg-black-800 text-3xl underline">Home</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
