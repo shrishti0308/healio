bfaac994a656602b682e061929e825f2f6f9c945
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-09-07 13:12:56 +0530

    Add PassKeyModal display for admin users in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { is } from "zod/v4/locales";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PassKeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
