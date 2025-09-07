943b94c4c973ea2c710c5ea8ac7eea2760f1f66a
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2025-09-07 13:11:51 +0530

    Add AppointmentModal component for scheduling and canceling appointments

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,58 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { text } from "stream/consumers";
+import AppointmentForm from "./forms/AppointmentForm";
+import { Appointment } from "@/types/appwrite.types";
+import { set } from "zod";
+
+const AppointmentModal = ({
+  type,
+  patientId,
+  userId,
+  appointment,
+}: {
+  type: "schedule" | "cancel";
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+        >
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-4">
+          <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
+          <DialogDescription>
+            Please fill in the following details to {type} an appointment
+          </DialogDescription>
+        </DialogHeader>
+
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          appointment={appointment}
+          type={type}
+          setOpen={setOpen}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AppointmentModal;
